f970f5dd4ab864f6580f5878a8c0979336653e32
Apiquet <anthony.piquet2@gmail.com>
2018-11-04 15:01:13 +0100

    Adding ML functions and test function

---
+++ TP3-KNN-Assignment/Part1/test_knn_2D.m
@@ -0,0 +1,78 @@
+clear; 
+close all; 
+clc;
+
+dataset_path = '../../TP3-KNN-Datasets/';
+rng(42);
+seed = rng;
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%           1) Load 2D KNN Function Testing Dataset          %%
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Load Concentric Circle Data
+% Load Dataset
+load(strcat(dataset_path,'/2d-concentric-circles.mat'))
+[~, M] = size(X);
+rand_idx = randperm(M);
+X = X(:,rand_idx);
+y = y(rand_idx);
+y = y + 1;
+
+% Visualize Dataset
+options.labels      = y;
+options.class_names = {'y = 1','y = 2'};
+options.title       = '2D Concentric Circles Dataset';
+
+h0 = ml_plot_data(X',options);
+axis equal
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%     2) Data Handling for Classification (split_data.m)        %%
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Select Training/Testing Ratio
+valid_ratio = 0.8;
+
+% Split data into a training dataset that kNN can use to make predictions 
+% and a test dataset that we can use to evaluate the accuracy of the model.
+[X_train, y_train, X_test, y_test] = split_data(X, y, valid_ratio);
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%     3) Visualize kNN Results and Accuracy            %%
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Select k
+params.k = 2; 
+params.d_type = 'L2';
+
+% Compute y_estimate from k-NN
+y_est =  my_knn(X_train, y_train, X_test, params);
+
+% Compute Accuracy
+acc =  my_accuracy(y_test, y_est);
+
+% Visualize Split Dataset
+options.labels      = y;
+options.class_names = [];
+options.title       = sprintf('My kNN, valid-ratio = %1.2f, k= %d, Acc = %1.3f',valid_ratio, params.k, acc);
+h2 = ml_plot_data(X',options); hold on;
+scatter(X_test(1,y_est == 1),X_test(2,y_est == 1),150,'o','MarkerEdgeColor', [1 0 0]);hold on;
+scatter(X_test(1,y_est == 2),X_test(2,y_est == 2),150,'o','MarkerEdgeColor', [0 0 1]);
+legend({'$y=1$','$y = 2$','$\hat{y} = 1$','$\hat{y} = 2$'},'Interpreter','latex')
+axis equal
+
+% Plot K-NN Decision boundary
+knn_options.k      = params.k;
+knn_options.d_type = params.d_type;
+[~, model]= knn_classifier(X_train, y_train, [], knn_options);
+f_knn     = @(X_test)knn_classifier(X_test, [], model, []);
+
+% Plot Decision Boundary
+c_options         = [];
+plot_data_options = [];
+c_options.dim_swaped     = true;
+c_options.show_misclass  = false;
+c_options.title          = sprintf('K(%d)-NN Decision Boundary with valid-ratio: %1.2f',params.k,valid_ratio);
+if exist('hc','var') && isvalid(hc), delete(hc);end
+hc = ml_plot_classifier(f_knn,X',y,c_options,plot_data_options);
+axis tight
